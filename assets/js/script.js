818e23fb4922d51a83a9b5b4ad6d5c12b508f498
Callum Mackintosh <callum.e.s.mackintosh@gmail.com>
2022-12-22 21:14:08 +1100

    Added HTML coding back, updated JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -33,33 +33,6 @@ currentDay.textContent = formattedDate;
 
 const timeBlocks = document.querySelectorAll(".time-block");
 
-const container = document.querySelector(".container-lg");
-
-for (let i = 7; i <= 19; i++) {
-  const timeBlock = document.createElement("div");
-  timeBlock.id = `hour-${i}`;
-  timeBlock.classList.add("row", "time-block");
-
-  const hour = document.createElement("div");
-  hour.classList.add("col-2", "col-md-1", "hour", "text-center", "py-3");
-  hour.textContent = `${i}AM`;
-  timeBlock.appendChild(hour);
-
-  const description = document.createElement("textarea");
-  description.classList.add("col-8", "col-md-10", "description");
-  description.rows = 3;
-  timeBlock.appendChild(description);
-
-  const saveBtn = document.createElement("button");
-  saveBtn.classList.add("btn", "saveBtn", "col-2", "col-md-1");
-  saveBtn.setAttribute("aria-label", "save");
-  saveBtn.innerHTML = '<i class="fas fa-save" aria-hidden="true"></i>';
-  timeBlock.appendChild(saveBtn);
-
-  container.appendChild(timeBlock);
-}
-
-
 timeBlocks.forEach(timeBlock => {
   const hour = timeBlock.id.split("-")[1];
   if (hour < today.getHours()) {
